77591b8f2cc47926423ceadb7b1e59fe29d91fd7
rodrigomes002 <rodrigomes002@gmail.com>
2023-11-14 20:39:28 -0300

    criando componentes de formulário

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import LoginCreate from "./LoginCreate";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordRest from "./LoginPasswordRest";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/create" element={<LoginCreate />} />
+        <Route path="/lost-password" element={<LoginPasswordLost />} />
+        <Route path="/reset-password" element={<LoginPasswordRest />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/Components/Login/LoginPasswordRest.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPasswordRest = () => {
+  return <div>form</div>;
+};
+
+export default LoginPasswordRest;
